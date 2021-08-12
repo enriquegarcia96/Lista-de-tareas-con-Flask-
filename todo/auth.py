e95f10c37b770cfebd9d0ad9794106193e3a07cd
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-08-12 17:25:13 -0600

    feat: 🎸 funciones para poteger las rutas

--- todo/auth.py
+++ todo/auth.py
@@ -2,12 +2,13 @@ import functools #funciones
 
 from flask import (
     Blueprint, flash, #envia mensajes genericos a las plantillas
-    g, render_template, request, url_for, session
+    g, render_template, request, url_for, session, redirect
 )
 
 #-> verificar si la contraseña es la correcta y (generate) incripta la contraseña
 from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug.utils import redirect 
+from werkzeug.utils import redirect
+from todo import db 
 
 from todo.db import get_db
 
@@ -25,7 +26,7 @@ def register():
         db, c = get_db()
         error = None
         c.execute(
-            'select id from user where username = %s'
+            'select id from user where username = %s', (username,)
         )
 
         #-> asigno los mensajes de error a la variable 'error' tipo flash
@@ -52,6 +53,7 @@ def register():
 
     return render_template('auth/register.html')#-> solo se renderiza si el usuario manda una peticion de tipo 'GET'
 
+
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -73,7 +75,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id'] #le asigno el id del usuario de la base de datos 
-            return redirect(url_for('index'))
+            return redirect(url_for('todo.index'))
 
         flash(error)
     return render_template('auth/login.html')
@@ -82,6 +84,37 @@ def login():
 
 
 
+#-> coloca el usuario dentro de la variable 'g'
+@bp.before_app_request
+def load_logged_in_user(): #-> se encarga de cargar el usuario
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'select * from user where id = %s', (user_id,)
+        )
+        g.user = c.fetchone()#-> devuelve el primer elemento que encuentre en la base de datos 
+
+
+
+#-> funcion que protege las rutas
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:#pregunta si existe el usuario dentro de la variable global
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+    return wrapped_view
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
 
 
 
